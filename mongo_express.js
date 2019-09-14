d3c51a16cc329ef9f418b7f2085011d2b4581ef4
Luiz Baroncello <luizfelipe_rv97@hotmail.com>
2019-09-14 18:15:46 -0300

    Disciplina refatorada
    Disciplina refatorada para ter clean code

--- mongo_express.js
+++ mongo_express.js
@@ -14,20 +14,9 @@ const dbName = "matriculas";
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res) {
-    renderizaPaginaInicial(res);
-});
-
-function renderizaPaginaInicial(res) {
-    buscaAlunos()
-        .toArray(function (err, alunos) {
-            buscaDisciplinas()
-                .toArray(function (err, disciplinas) {
-                    res.render("home", {title: "Inicio", listaAlunos: alunos, listaDisciplinas: disciplinas});
-                });
-        });
-}
-
+/////////////////////////////////////////////
+//Banco
+//////////////////////////////////////////////
 function buscaAlunos() {
     return client
             .db(dbName)
@@ -56,6 +45,13 @@ function insereAlunoNoBanco(nome, matricula) {
         .insertOne({nome: nome, matriculaAluno: matricula});
 }
 
+function insereDisciplinaNoBanco(nome, codigo, horariosDiciplinas) {
+    client
+        .db(dbName)
+        .collection("disciplina")
+        .insertOne({nome: nome, codigoDisciplina: codigo, horarios: horariosDiciplinas});
+}
+
 function atualizaAlunoNoBanco(nome, matricula) {
     client
     .db(dbName)
@@ -76,6 +72,51 @@ function atualizaAlunoNoBanco(nome, matricula) {
         });
 }
 
+function atualizaDisciplinaNoBanco(codigo, nome, res) {
+    client
+    .db(dbName)
+    .collection("disciplina")
+    .updateOne(
+        {
+            codigoDisciplina: codigo
+        },
+        {
+            $set: {nome: nome}
+        },
+        function (err, result) {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.redirect("/");
+            return;
+        });
+}
+
+function buscaDisciplinaPorCodigo(codigo) {
+    return client
+            .db(dbName)
+            .collection("disciplina")
+            .findOne({codigoDisciplina: codigo})
+}
+
+/////////////////////////////////////////////
+//Lógica
+//////////////////////////////////////////////
+app.get("/", function(req, res) {
+    renderizaPaginaInicial(res);
+});
+
+function renderizaPaginaInicial(res) {
+    buscaAlunos()
+        .toArray(function (err, alunos) {
+            buscaDisciplinas()
+                .toArray(function (err, disciplinas) {
+                    res.render("home", {title: "Inicio", listaAlunos: alunos, listaDisciplinas: disciplinas});
+                });
+        });
+}
+
 app.get("/registerAluno", function(req, res) {
     res.render("registerAluno", {
         title: "Novo aluno",
@@ -87,7 +128,7 @@ app.get("/registerAluno/:matriculaAluno", function(req, res) {
     buscaAlunoPorMatricula(req.params.matriculaAluno)
         .then(function (aluno) {
             if (!aluno){
-                res.render("naoEncontrado", {title: "Não Encontrado"});
+                renderizaTelaNotFound(res);
             } else {
                 res.render("registerAluno", {
                     title: "Alterar aluno", 
@@ -105,8 +146,25 @@ function validateAluno(nome, matricula, res) {
     return true;
 }
 
+function validateDisciplina(nome, codigo, qtdHorariosSelecionados, res) {
+    if (!nome || !codigo || (qtdHorariosSelecionados < 1 || qtdHorariosSelecionados > 4)) {
+        renderizaTelaBadRequest(res);
+        return false;
+    }
+
+    return true;
+}
+
+function renderizaTelaDadoJaCadastrado(res) {
+    res.render("dadoJaCadastrado", {codigo: codigo, title: "Dado já cadastrado"});
+}
+
 function renderizaTelaBadRequest(res) {
     res.render("badRequest", {title: "Requisição inválida"});
+}
+
+function renderizaTelaNotFound(res) {
+    res.render("naoEncontrado", {title: "Não Encontrado"});
 }
 
 function cadastraNovoAluno(req, res) {
@@ -115,7 +173,7 @@ function cadastraNovoAluno(req, res) {
     buscaAlunoPorMatricula(matricula)
        .then(function(aluno) {
             if(aluno) {
-                res.render("dadoJaCadastrado", {codigo: matricula, title: "Dado já cadastrado"});
+                renderizaTelaDadoJaCadastrado(res);
             } else {
                 let nome = req.body.nome.trim();
                 let isAlunoValido = validateAluno(nome, matricula, res);
@@ -152,10 +210,6 @@ app.post("/registerAluno/:matricula?", function(req, res) {
     }
 }); 
 
-/////////////////////////////////////////////
-//DISCIPLINA
-//////////////////////////////////////////////
-
 app.get("/registerDisciplina", function(req, res) {
     res.render("registerDisciplina", {
         title: "Nova disciplina",
@@ -164,15 +218,11 @@ app.get("/registerDisciplina", function(req, res) {
 });
 
 app.get("/registerDisciplina/:codigoDisciplina", function(req, res) {
-    client
-        .db(dbName)
-        .collection("disciplina")
-        .findOne({codigoDisciplina: req.params.codigoDisciplina})
+    buscaDisciplinaPorCodigo(req.params.codigoDisciplina)
         .then(function (disciplina) {
-            if (!disciplina)
-                res.render("naoEncontrado", {title: "Não Encontrado"});
-            else {
-                console.table(disciplina.horarios);
+            if (!disciplina){
+                renderizaTelaNotFound(res);
+            } else {
                 res.render("registerDisciplina", {
                     title: "Alterar disciplina", 
                     disciplina: disciplina
@@ -181,99 +231,73 @@ app.get("/registerDisciplina/:codigoDisciplina", function(req, res) {
         });
 });
 
+function criaMatrizHorariosSelecionados(req) {
+    const horariosSegunda = [req.body.seg1, req.body.seg2, req.body.seg3, req.body.seg4];
+    const horariosTerca = [req.body.ter1, req.body.ter2, req.body.ter3, req.body.ter4];
+    const horariosQuarta = [req.body.quat1, req.body.quat2, req.body.quat3, req.body.quat4];
+    const horariosQuinta = [req.body.quint1, req.body.quint2, req.body.quint3, req.body.quint4];
+    const horariosSexta = [req.body.sex1, req.body.sex2, req.body.sex3, req.body.sex4];
 
-app.post("/registerDisciplina/:codigoDisciplina?", function(req, res) {
+    const horarios = [horariosSegunda, horariosTerca, horariosQuarta, horariosQuinta, horariosSexta];
 
-    if (!req.params.codigoDisciplina) {
-        var codigo = req.body.codigoDisciplina.trim();
-        
-        client
-        .db(dbName)
-        .collection("disciplina")
-        .findOne({codigoDisciplina: codigo})
-        .then(function(disciplina){
+    return horarios;
+}
 
+function cadastraNovaDisciplina(req, res) {
+    const codigo = req.body.codigoDisciplina.trim();
+
+    buscaDisciplinaPorCodigo(codigo)
+        .then(function(disciplina) {
             if(disciplina) {
-                res.render("dadoJaCadastrado", {codigo: codigo, title: "Dado já cadastrado"});
-            } else{
-                let nome = req.body.nome.trim();
+                renderizaTelaDadoJaCadastrado(res);
+            } else {
+                const nome = req.body.nome.trim();
+                const horarios = criaMatrizHorariosSelecionados(req);
+                let qtdHorariosSelecionados = 0;
+                let horariosDiciplinas = [];
 
-                let horariosSegunda = [req.body.seg1, req.body.seg2, req.body.seg3, req.body.seg4];
-                let horariosTerca = [req.body.ter1, req.body.ter2, req.body.ter3, req.body.ter4];
-                let horariosQuarta = [req.body.quat1, req.body.quat2, req.body.quat3, req.body.quat4];
-                let horariosQuinta = [req.body.quint1, req.body.quint2, req.body.quint3, req.body.quint4];
-                let horariosSexta = [req.body.sex1, req.body.sex2, req.body.sex3, req.body.sex4];
-
-                let horarios = [horariosSegunda, horariosTerca, horariosQuarta, horariosQuinta, horariosSexta];
-
-                let diaSemana = 0;
-                let horariosPosicao = 0;
-                let qtdSelecionados = 0;
-                
-                let posicoesHorariosDisciplina = [];
-
-                horarios.forEach(function(horariosDias){ 
-                    horariosDias.forEach(function(horarioDefinido){
-                        if(horarioDefinido){
-                            qtdSelecionados++;
-                            posicoesHorariosDisciplina.push({diaSemana: diaSemana, horario: horariosPosicao});
-                        }              
-                        horariosPosicao++;          
-                    })
-                    diaSemana++; 
-                    horariosPosicao = 0;
-                }) ;
-
-                if (!nome || !codigo || (qtdSelecionados < 1 || qtdSelecionados > 4)) {
-                    res.render("badRequest", {title: "Requisição inválida"});
-                    return;
+                for (diaSemana = 0; diaSemana < horarios.length; diaSemana++) {
+                    for (horarioAula = 0; horarioAula < horarios[diaSemana].length; horarioAula++) {
+                        if(horarios[diaSemana][horarioAula]){
+                            qtdHorariosSelecionados++;
+                            horariosDiciplinas.push({diaSemana: diaSemana, horario: horarioAula});
+                        }
+                    }
                 }
 
-                client
-                    .db(dbName)
-                    .collection("disciplina")
-                    .insertOne({nome: nome, codigoDisciplina: codigo, horarios: posicoesHorariosDisciplina});
-                res.redirect("/");
+                const isDisciplinaValida = validateDisciplina(nome, codigo, qtdHorariosSelecionados, res);
+                
+                if (isDisciplinaValida) {
+                    insereDisciplinaNoBanco(nome, codigo, horariosDiciplinas);
+                    res.redirect("/");
+                } else {
+                    renderizaTelaBadRequest(res);
+                }
             }
         }
-
     );
+}
 
-       }
-    else {
-        var nome = req.body.nome.trim();
-        var codigo = req.params.codigoDisciplina;
+function atualizaDisciplina(req, res) {
+    const nome = req.body.nome.trim();
+    const codigo = req.params.codigoDisciplina;
 
-        if (!nome) {
-            res.render("badRequest", {title: "Requisição inválida"});
-            console.log("Nome não informado...")
-            return;
-        }
+    if (!nome) {
+        renderizaTelaBadRequest(res)
+        return;
+    } else {
+        atualizaDisciplinaNoBanco(codigo, nome, res);
+    }
 
-        //Realiza o UPDATE no banco.
-        console.log("update...")
-            client
-            .db(dbName)
-            .collection("disciplina")
-            .updateOne(
-                {
-                    codigoDisciplina: codigo
-                },
-                {
-                    $set: {nome: nome}
-                },
-                function (err, result) {
-                    if (err) {
-                        console.log(err);
-                        throw err;
-                    }
-                    res.redirect("/");
-                    return;
-                });
+}
+
+app.post("/registerDisciplina/:codigoDisciplina?", function(req, res) {
+    if (!req.params.codigoDisciplina) {
+        cadastraNovaDisciplina(req, res);
+    } else {
+        atualizaDisciplina(req, res);
     }
 }); 
-
-
 
 client.connect(function(err, db) {
     app.listen(port, function () {
